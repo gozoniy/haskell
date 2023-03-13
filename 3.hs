a4a3c41923b3fe473fd52efe7fb8c7adf2b5ae31
1gone <soba4ka808@gmail.com>
2023-03-13 22:22:50 +0700

    3 lab edit

--- 3.hs
+++ 3.hs
@@ -24,5 +24,27 @@ quicksort (x:xs) = quicksort(filter (<x) xs) ++ [x] ++ quicksort (filter (>=x) x
 bSort a [] = []
 bSort a (x:xs) = bSort a (filter (a x) xs) ++ [x] ++ bSort a (filter (not . a x) xs)
 
+-- Вторая часть Задание 12
+-- 2.12.Функция, которая меняет знак всех положительных элементов списка чисел, 
+-- например: по [-1, 0, 5, -10, -20] дает [-1,0,-5,-10,-20]
+
+negative :: [Int] -> [Int]
+negative [] = []
+negative (x:xs) = if (x > 0) then (-x):negative xs
+else x:negative xs
+
+-- Вторая часть Задание 4
+-- 2.4.Функция вычленения n -го элемента из заданного списка. 
+
+
+
+clr (x:xs) n n0 = if n0 /= n then x:clr xs n (n0+1)
+else xs
+del [] _ = []
+del l n = clr l n 0
+
+
+
+
 
 main = print (bSort (<) [1,2,5,1])
